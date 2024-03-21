9435549fdacdd019e616489c423fc05eca1d2966
Akki7247-ak <62496699+Akki7247-ak@users.noreply.github.com>
2024-03-21 15:42:03 +0000

    datatypes

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,19 @@
+const accountId = 111
+let accountEmail = "akki@gmail.com"
+var accountPassword ="1111"
+
+accountCity = "Jaipur"
+
+let accountState;
+
+/*
+ we are not using var beacouse 
+of block scope and functional scope
+*/
+
+// accountId = 123
+accountEmail = "pre@gmail.com"
+accountPassword = "12345"
+accountCity ="bhopal"
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,19 @@
+"use strict" //treat all js code as newer version
+
+//alert(6+9) // we are using node js not brower
+
+let name ="akhilesh"
+let age ="23"
+let req = true
+let password;
+
+// string => ""
+// bigint 
+// number => 2 to power 53
+// boolean => true/false
+// null 
+// undefined
+// sybol => unique 
+
+console.log(typeof null); //obeject showing
+console.log(typeof undefined);  // undefined showing
