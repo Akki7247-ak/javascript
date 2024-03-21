4190e6d84e9f6badaee58d8db0408f9c348ba038
Akki7247-ak <62496699+Akki7247-ak@users.noreply.github.com>
2024-03-21 17:54:58 +0000

    verssion of datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,31 @@
+
+let score = "akhilesh"
+
+console.log(typeof score);
+console.log(typeof (score));
+
+let valueNumber = Number(score)
+console.log(typeof (valueNumber));
+console.log(valueNumber);
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1 / false => 0
+
+
+let isLoggedin = 1
+
+let booleanIsLoggedIn = Boolean(isLoggedin)
+
+console.log(booleanIsLoggedIn);
+
+// 1 => true
+// 0 => false
+// "" => false
+// "akhilesh" => true
+
+let someNumber = 33
+let stringNumber = String(someNumber)
+
+console.log(stringNumber);
+console.log(typeof stringNumber);
